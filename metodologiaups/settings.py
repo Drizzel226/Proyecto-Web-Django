31a401d08024706e1ed7195dac46f04573aab46a
ElGaja <diegoatun100@gmail.com>
2024-09-11 16:23:59 -0300

    inicio de sesion para ver todo

--- metodologiaups/settings.py
+++ metodologiaups/settings.py
@@ -3,24 +3,15 @@ import os
 from dotenv import load_dotenv
 load_dotenv() 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
-
 if not SECRET_KEY:
     raise ValueError("Falta la clave secreta en las variables de entorno")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -68,7 +59,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'metodologiaups.wsgi.application'
 
-# Database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -76,7 +66,6 @@ DATABASES = {
     }
 }
 
-# Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -92,7 +81,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalization
 LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'America/Santiago'
@@ -101,38 +89,30 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'miapp', 'static'),
 ]
 
-# Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 # Authentication settings
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',  # Django's default authentication
     'social_core.backends.google.GoogleOAuth2',  # Google OAuth2 backend
-    
 )
-
-
 
 # Google OAuth2 settings
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
 
-# Verificar que las variables de entorno estén configuradas
 if not SOCIAL_AUTH_GOOGLE_OAUTH2_KEY or not SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET:
     raise ValueError("Faltan las credenciales de OAuth en las variables de entorno")
 
-
 # URL to redirect after login
+LOGIN_URL = '/login/'  # Establecer la URL de inicio de sesión real
 LOGIN_REDIRECT_URL = '/'  # Ajusta esto según tu necesidad
 LOGOUT_REDIRECT_URL = '/'
 
 # Additional settings for social-auth-app-django
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
-
--- miapp/views.py
+++ miapp/views.py
@@ -1,18 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 def inicio(request):
+    # Permitir que todos vean el menú principal
     return render(request, 'miapp/inicio.html')
 
+@login_required
 def miniproyectos(request):
     return render(request, 'miapp/miniproyectos.html')
 
+@login_required
 def kaizer(request):
     return render(request, 'miapp/kaizer.html')
 
-
-def inicio(request):
-    context = {}
-    if request.user.is_authenticated:
-        context['user_name'] = request.user.first_name  # o request.user.username o request.user.get_full_name()
-    return render(request, 'miapp/inicio.html', context)
-
+def pagina_no_autorizado(request):
+    return render(request, 'miapp/no_autorizado.html')
